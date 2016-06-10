01b8728cca74faf0eaac002ab2b493ae929b2bff
Jens Hadlich <jenshadlich@googlemail.com>
2016-06-10 12:48:40 +0200

    play with loggers

--- src/main/java/de/jeha/demo/springboot/resources/HelloResource.java
+++ src/main/java/de/jeha/demo/springboot/resources/HelloResource.java
@@ -2,6 +2,8 @@ package de.jeha.demo.springboot.resources;
 
 import de.jeha.demo.springboot.api.HelloResponse;
 import de.jeha.demo.springboot.dao.HelloDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,15 @@ import java.io.IOException;
 @RestController
 public class HelloResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloResource.class);
+
     @Autowired
     private HelloDAO helloDAO;
 
     @RequestMapping(value = "/hello", produces = "application/json")
     public HelloResponse hello() throws IOException {
+        LOG.info("hello@info");
+        LOG.debug("hello@debug");
         return new HelloResponse(helloDAO.getHelloString());
     }
 
