fcce2536d42e63b5e1a4aeb83b4df6d527323544
Jens Hadlich <jenshadlich@googlemail.com>
2016-11-04 11:23:18 +0100

    update

--- src/main/java/de/jeha/demo/springboot/resources/ThrowExceptionResource.java
+++ src/main/java/de/jeha/demo/springboot/resources/ThrowExceptionResource.java
@@ -1,5 +1,7 @@
 package de.jeha.demo.springboot.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,13 @@ import java.io.IOException;
 @RestController
 public class ThrowExceptionResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ThrowExceptionResource.class);
+
     @RequestMapping(value = "/throwException", produces = "application/json")
     public String throwException() throws IOException {
-        throw new RuntimeException("This exception was thrown by intention!");
+        RuntimeException e = new RuntimeException("This exception was thrown by intention!");
+        LOG.error("", e);
+        throw e;
     }
 
 }
